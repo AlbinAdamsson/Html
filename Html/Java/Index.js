2f4f3bf7ae30674457bc814e552c6fda8c86f4d1
Albin <adamsonalbin@gmail.com>
2023-12-13 09:32:53 +0100

    Fruits lection 2

--- Html/Java/Index.js
+++ Html/Java/Index.js
@@ -1,1 +1,27 @@
-wefew
+
+let fruits = ["Apple", "Banana", "Orange"];
+console.log(fruits)
+fruits [0] = "fish"
+console.log(fruits)
+fruits [3] = "fish"
+console.log(fruits)
+console.log(fruits. length)
+
+fruits. push ("Grape")
+console.log(fruits)
+
+fruits.pop ()
+console.log(fruits)
+
+fruits.unshift ("Grape")
+console.log(fruits)
+
+fruits.shift ("Grape")
+console.log(fruits)
+
+Sfruits=fruits.toString()
+console.log(Sfruits)
+
+
+Jfruits=fruits.join("")
+console.log(Jfruits)
